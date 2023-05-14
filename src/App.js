b766b122bf39aa7faf30997a2dc4944d093bc3cb
Ryo SHIINA <ryo.shiina@starnetapp.com>
2023-05-15 05:38:30 +0900

    v1.0.0 - initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,209 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "jimp";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import UploadIcon from "@mui/icons-material/Upload";
+import ViewAgendaIcon from "@mui/icons-material/ViewAgenda";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { merge } from "./merger";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"UmaStitcher! - Developed by "}
+      <Link color="inherit" href="https://github.com/pooch-tonic">
+        pooch-tonic
+      </Link>
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 function App() {
+  const [result, setResult] = React.useState(null);
+  const [rawFiles, setRawFiles] = React.useState([]);
+  const [previewUrls, setPreviewUrls] = React.useState([]);
+  const [uploading, setUploading] = React.useState(false);
+  const [merging, setMerging] = React.useState(false);
+
+  const computeResult = async (imageBuffers) => {
+    const res = await merge(imageBuffers);
+    setResult(res);
+    setMerging(false);
+  };
+
+  const handleMerge = async () => {
+    const len = rawFiles?.length;
+    if (len > 1) {
+      setMerging(true);
+      setResult(null);
+      // WebWorker is a pain in the a** to implement, so just used setTimeout of 100ms to let React DOM update UI state before freezing with image manipulation
+      setTimeout(() => {
+        const reader = new FileReader();
+        const loadedBuffers = [];
+
+        // Need to make sure images are loaded in input order in sync
+        // FileReaderSync not available so made it by hand recursively with the onloadend event
+        const loadFile = (index, maxIndex, loadedUrls) => {
+          reader.onloadend = () => {
+            loadedBuffers.push(reader.result);
+            // if last image has been loaded
+            if (index === maxIndex) {
+              computeResult(loadedBuffers);
+            } else {
+              loadFile(index + 1, maxIndex, loadedUrls);
+            }
+          };
+          reader.readAsArrayBuffer(rawFiles[index]);
+        };
+        loadFile(0, len - 1, []);
+      }, 100);
+    } else {
+      console.log("no enough images to merge");
+    }
+  };
+
+  const handleUpload = (event) => {
+    const files = event?.target?.files;
+    if (files) {
+      setUploading(true);
+      const len = files.length;
+      if (len > 0) {
+        setPreviewUrls([]);
+        setResult(null);
+        setRawFiles([...files]);
+        const reader = new FileReader();
+
+        // Need to make sure images are loaded in input order in sync
+        // FileReaderSync not available so made it by hand recursively with the onloadend event
+        const loadFile = (index, maxIndex, loadedUrls) => {
+          reader.onloadend = () => {
+            loadedUrls.push(reader.result);
+            // if last image has been loaded
+            if (index === maxIndex) {
+              setPreviewUrls(loadedUrls);
+              setUploading(false);
+            } else {
+              loadFile(index + 1, maxIndex, loadedUrls);
+            }
+          };
+          reader.readAsDataURL(files[index]);
+        };
+        loadFile(0, len - 1, []);
+      } else {
+        console.log("0 files were uploaded.");
+      }
+    } else {
+      console.log("no files");
+    }
+  };
+
+  const renderBottomView = () => {
+    if (!!result) {
+      return (
+        <Box sx={{ mt: 2 }}>
+          <img src={result} alt="Merge result" loading="lazy" width="100%" />
+        </Box>
+      );
+    } else if (previewUrls?.length > 0) {
+      return (
+        <ImageList sx={{ mt: 2 }} cols={3}>
+          {" "}
+          {previewUrls.map((previewUrl, index) => (
+            <ImageListItem key={previewUrl}>
+              <img
+                src={previewUrl}
+                alt={`preview of uploaded screenshot no. ${index + 1}`}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        color="default"
+        elevation={0}
+        sx={{
+          position: "relative",
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}
+      >
+        <Toolbar>
+          <Avatar
+            sx={{ mr: 1 }}
+            alt="UmaInfoMerger logo"
+            src="/logo256.png"
+            variant="square"
+          />
+          <Typography variant="h6" color="inherit" noWrap>
+            UmaStitcher!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <input
+              accept="image/*"
+              style={{ display: "none" }}
+              id="raised-button-file"
+              multiple
+              type="file"
+              onInput={handleUpload}
+            />
+            <label htmlFor="raised-button-file">
+              <Button variant="outlined" component="span" disabled={uploading}>
+                {uploading ? (
+                  <CircularProgress size={24} sx={{ mr: 1 }} />
+                ) : (
+                  <UploadIcon sx={{ mr: 1 }} />
+                )}
+                {previewUrls?.length > 0 ? "Reupload" : "Upload"}
+              </Button>
+            </label>
+            {!result && previewUrls?.length > 1 && (
+              <Button
+                variant="contained"
+                sx={{ ml: 2 }}
+                onClick={handleMerge}
+                disabled={merging}
+              >
+                {merging ? (
+                  <CircularProgress size={24} sx={{ mr: 1 }} />
+                ) : (
+                  <ViewAgendaIcon sx={{ mr: 1 }} />
+                )}
+                Merge
+              </Button>
+            )}
+          </Box>
+          {renderBottomView()}
+        </Paper>
+        <Copyright />
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/merger.js
@@ -0,0 +1,397 @@
+const { Jimp } = window;
+
+const conf = {
+  /**
+   * hex color of bottom frame background (#F2F2F2 in hex, from iOS screenshot, without any filter)
+   */
+  bottomFrameBgColorRGBA: { r: 242, g: 242, b: 242, a: 255 },
+  /**
+   * height in percentage for the bottom frame lookup start point
+   */
+  firstLookUpHeight: 0.75,
+  /**
+   * limit in percentage indicating at what proportion of the image width we should stop looking up further.
+   * (i.e. if the lookup from the left side hasn't found any frame color pixel at x % of the image width, stop)
+   */
+  firstLookUpLimitWidth: 0.3,
+  /**
+   * limit in percentage indicating at what proportion of the image height we should stop looking up further.
+   * (i.e. if the lookup from inside the frame hasn't found any out-of frame color pixel at x % of the image height, stop)
+   */
+  firstLookUpLimitHeight: 0.9,
+  /**
+   * amount of repetitive pixels needed to be sure we hit the frame and not an accidental pixel
+   */
+  lookupChainSize: 3,
+  /**
+   * percentage of X offset to start pattern sampling based on the given image
+   *
+   * since the bottom frame is first cut out before being sent to the sampling
+   * function, please take in account the frame dimensions as a base reference
+   */
+  patternSampleXOffset: 0.25,
+  /**
+   * percentage width of sample, since specifying it with pixels might go wrong
+   * with different resolutions
+   *
+   * please take in account the bottom frame dimensions as a base reference
+   */
+  patternSampleWidth: 0.55,
+  /**
+   * percentage height of sample, since specifying it with pixels might go wrong
+   * with different resolutions
+   *
+   * please take in account the bottom frame dimensions as a base reference
+   */
+  patternSampleHeight: 0.11,
+  /**
+   * number of columns in the pattern = amount of pixel samples in one row
+   */
+  sampleCols: 16,
+  /**
+   * number of rows in the pattern = amount of pixel samples in one col
+   */
+  sampleRows: 4,
+};
+
+const init = () => {
+  const { r, g, b, a } = conf.bottomFrameBgColorRGBA;
+  conf["bottomFrameBgColorInt"] = Jimp.rgbaToInt(r, g, b, a);
+};
+
+init();
+
+// lookup function direction values
+const directions = {
+  RIGHT: 1,
+  LEFT: -1,
+  UP: -1,
+  DOWN: 1,
+};
+
+// lookup modes: inwards when trying to find frame from outside the frame, and outwards for the opposite
+const lookupModes = {
+  INWARDS: 0,
+  OUTWARDS: 1,
+};
+
+// lookup chain of pixels in row direction
+const lookupXChain = (
+  image,
+  intColor,
+  direction,
+  chainSize = 1,
+  startX = 0,
+  startY = 0,
+  limitX = 10,
+  lookupMode
+) => {
+  let matchRepeatCount = 0;
+  let matchedX = startX;
+  let colorCondFunc;
+
+  if (lookupMode === lookupModes.INWARDS) {
+    colorCondFunc = (col) => col !== intColor;
+  } else {
+    colorCondFunc = (col) => col === intColor;
+  }
+
+  for (let x = startX; x !== limitX; x += direction) {
+    const pixelColor = image.getPixelColor(x, startY);
+    if (colorCondFunc(pixelColor)) {
+      if (matchRepeatCount > 0) {
+        matchRepeatCount = 0;
+      }
+    } else {
+      matchRepeatCount++;
+      if (matchRepeatCount === chainSize) {
+        matchedX = x + -direction * (matchRepeatCount - 1);
+        break;
+      }
+    }
+  }
+  return matchedX;
+};
+
+// lookup chain of pixels in col direction
+const lookupYChain = (
+  image,
+  intColor,
+  direction,
+  chainSize = 1,
+  startX = 0,
+  startY = 0,
+  limitY = 10,
+  lookupMode
+) => {
+  let matchRepeatCount = 0;
+  let matchedY = startY;
+  let colorCondFunc;
+
+  if (lookupMode === lookupModes.INWARDS) {
+    colorCondFunc = (col) => col !== intColor;
+  } else {
+    colorCondFunc = (col) => col === intColor;
+  }
+
+  for (let y = startY; y !== limitY; y += direction) {
+    const pixelColor = image.getPixelColor(startX, y);
+    if (colorCondFunc(pixelColor)) {
+      if (matchRepeatCount > 0) {
+        matchRepeatCount = 0;
+      }
+    } else {
+      matchRepeatCount++;
+      if (matchRepeatCount === chainSize) {
+        matchedY = y + -direction * (matchRepeatCount - 1);
+        break;
+      }
+    }
+  }
+  return matchedY;
+};
+
+// get the coordinates of the bottom frame and crop it out
+const getBottomFrameCoords = (image) => {
+  const { width, height } = image?.bitmap;
+
+  // start from left side at the first lookup height coordinate to find the bottom frame.
+  const firstLookUpY = Math.round(conf.firstLookUpHeight * height);
+  // if bottom frame
+  const limitXLeft = Math.round(conf.firstLookUpLimitWidth * width);
+  const limitXRight = width - limitXLeft;
+  const limitYBottom = Math.round(conf.firstLookUpLimitHeight * height);
+  const limitYTop = height - limitYBottom;
+  const leftX = lookupXChain(
+    image,
+    conf["bottomFrameBgColorInt"],
+    directions.RIGHT,
+    conf.chainSize,
+    0,
+    firstLookUpY,
+    limitXLeft,
+    lookupModes.INWARDS
+  );
+  const rightX = lookupXChain(
+    image,
+    conf["bottomFrameBgColorInt"],
+    directions.LEFT,
+    conf.chainSize,
+    width - 1,
+    firstLookUpY,
+    limitXRight,
+    lookupModes.INWARDS
+  );
+  const topY = lookupYChain(
+    image,
+    conf["bottomFrameBgColorInt"],
+    directions.UP,
+    conf.chainSize,
+    leftX,
+    firstLookUpY,
+    limitYTop,
+    lookupModes.OUTWARDS
+  );
+  const bottomY = lookupYChain(
+    image,
+    conf["bottomFrameBgColorInt"],
+    directions.DOWN,
+    conf.chainSize,
+    leftX,
+    firstLookUpY,
+    limitYBottom,
+    lookupModes.OUTWARDS
+  );
+  return {
+    leftX,
+    rightX,
+    topY,
+    bottomY,
+    width: rightX - leftX,
+    height: bottomY - topY,
+  };
+};
+
+// sample a pattern based on config values
+const samplePattern = (image, patternMap, offsetX = 0, offsetY = 0) => {
+  const samples = [];
+  patternMap.coords.forEach(([x, y]) => {
+    samples.push(image.getPixelColor(offsetX + x, offsetY + y));
+  });
+  return samples;
+};
+
+// print pattern on image, only used for dev testing purposes
+const showPattern = (image, patternMap, offsetX = 0, offsetY = 0) => {
+  patternMap.coords.forEach(([x, y]) => {
+    image.setPixelColor(65535, offsetX + x, offsetY + y);
+  });
+};
+
+// look for the pattern by keeping the same X coord and moving on the Y axis from top (0) to given Y limit
+// return a value between 0 and image height - pattern height if a match is found, otherwise return -1
+const findPattern = (
+  image,
+  patternMap,
+  targetPattern,
+  baseX = 0,
+  baseY = 0,
+  limitY
+) => {
+  // implement a custom interpretation of SAD algorithm, could be switched to SSD if not enough
+  let res = null;
+
+  for (let currY = baseY; currY < limitY; currY++) {
+    let sad = 0;
+    // dev perf test
+    // let passes = 0;
+
+    // eslint-disable-next-line no-loop-func
+    patternMap.coords.every(([x, y], i) => {
+      // dev test
+      // passes++;
+      const currentPixel = image.getPixelColor(x + baseX, y + currY);
+      if (currentPixel !== targetPattern[i]) {
+        const currentPixelRGBA = Jimp.intToRGBA(currentPixel);
+        const targetPixelRGBA = Jimp.intToRGBA(targetPattern[i]);
+        sad +=
+          Math.abs(targetPixelRGBA.r - currentPixelRGBA.r) +
+          Math.abs(targetPixelRGBA.g - currentPixelRGBA.g) +
+          Math.abs(targetPixelRGBA.b - currentPixelRGBA.b) +
+          Math.abs(targetPixelRGBA.a - currentPixelRGBA.a);
+        if (res && sad > res.sad) {
+          // stop the search if the error gets bigger than what's already on record
+          return false;
+        }
+      }
+      // image.setPixelColor(65535, x + baseX, y + currY);
+      return true;
+    });
+    if (!res || sad < res.sad) {
+      res = { y: currY, sad };
+    }
+    // for debug & perf check
+    // console.log(currY, passes, res);
+  }
+  return res;
+};
+
+// create an array indicating the pixel sampling coordinates defining our pattern, assuming all images to sample on are of the same size
+const createPatternMap = (baseImageWidth, baseImageHeight) => {
+  const height = Math.floor(baseImageHeight * conf.patternSampleHeight);
+  const width = Math.floor(baseImageWidth * conf.patternSampleWidth);
+  const spacingX = Math.floor(width / conf.sampleCols);
+  const spacingY = Math.floor(height / conf.sampleRows);
+  const coords = [];
+  for (let row = 0; row < conf.sampleRows; row++) {
+    for (let col = 0; col < conf.sampleCols; col++) {
+      coords.push([col * spacingX, row * spacingY]);
+    }
+  }
+  return {
+    coords,
+    spacingX,
+    spacingY,
+    baseHeight: height,
+    baseWidth: width,
+    actualHeight: (conf.sampleRows - 1) * spacingY + 1,
+    actualWidth: (conf.sampleCols - 1) * spacingX + 1,
+  };
+};
+
+// use sampling to get overlapping parts between frames and crop accordingly
+const cropBottomFrames = async (images, bottomFrameCoords) => {
+  const { width, height, leftX, rightX, topY, bottomY } = bottomFrameCoords;
+  const patternMap = createPatternMap(width, height);
+  const patternXOffset = Math.round(width * conf.patternSampleXOffset + leftX);
+  // used when getting most-bottom pattern
+  const patternYOffset = bottomY - patternMap.actualHeight;
+
+  // holds Y values to where to crop (= discard top part of frame) for each image, knowing the first image has to be full
+  const cropYValues = [topY];
+
+  // use pattern matching to know wheere to crop for stitching
+  for (let i = 1; i < images.length; i++) {
+    const prev = images[i - 1];
+    const curr = images[i];
+    const prevBottomPattern = samplePattern(
+      prev,
+      patternMap,
+      patternXOffset,
+      patternYOffset
+    );
+
+    // send the first sample Y Offset as the max Y limit for sampling
+    const patternMatch = findPattern(
+      curr,
+      patternMap,
+      prevBottomPattern,
+      patternXOffset,
+      topY,
+      patternYOffset
+    );
+
+    cropYValues.push(patternMatch.y);
+  }
+
+  cropYValues.forEach((y, i) => {
+    images[i].crop(
+      leftX,
+      y + patternMap.actualHeight,
+      width,
+      bottomY - y - patternMap.actualHeight
+    );
+  });
+
+  // for testing in dev env
+  // showPattern(bottomFrames[0], patternMap, patternXOffset, patternYOffset);
+};
+
+// read image buffers as JIMP images then extract bottom frames and finally stitch them together
+const combineImages = async (images) => {
+  const jimpImages = await Promise.all(
+    images.map(async (currentImage) => {
+      const currentJimpImage = await Jimp.read(currentImage);
+      return currentJimpImage;
+    })
+  );
+  const bottomFrameCoords = getBottomFrameCoords(jimpImages[0]);
+
+  cropBottomFrames(jimpImages, bottomFrameCoords);
+
+  const finalSize = {
+    w: jimpImages[0].bitmap.width,
+    h: jimpImages.reduce((acc, img) => acc + img.bitmap.height, 0),
+  };
+
+  const stitch = async (
+    baseImage,
+    overlayImages,
+    currentIndex,
+    baseImageStitchY
+  ) => {
+    const currentImage = overlayImages[currentIndex];
+    const newImage = await baseImage.blit(currentImage, 0, baseImageStitchY);
+    if (overlayImages[currentIndex + 1]) {
+      return stitch(
+        newImage,
+        overlayImages,
+        currentIndex + 1,
+        baseImageStitchY + currentImage.bitmap.height
+      );
+    }
+    return newImage;
+  };
+
+  let res = await new Jimp(finalSize.w, finalSize.h);
+  res = await stitch(res, jimpImages, 0, 0);
+
+  return res;
+};
+
+// exposed merge function to call the stitching function then convert the resulting JIMP image to base64
+export const merge = async (images) => {
+  const resultJimpImage = await combineImages(images);
+  const resultImage = await resultJimpImage.getBase64Async(Jimp.AUTO);
+  return resultImage;
+};
